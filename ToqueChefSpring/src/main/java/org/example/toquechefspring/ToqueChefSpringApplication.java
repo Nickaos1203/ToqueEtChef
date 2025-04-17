fa9cf00a76a5afb46c6ac793d9dd3f587a4aab71
Nickaos1203 <nicolas.guillon95@yahoo.com>
2025-04-17 22:58:30 +0200

    spring app init

---
+++ ToqueChefSpring/src/main/java/org/example/toquechefspring/ToqueChefSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.toquechefspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToqueChefSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToqueChefSpringApplication.class, args);
+	}
+
+}
---
+++ ToqueChefSpring/src/test/java/org/example/toquechefspring/ToqueChefSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.toquechefspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToqueChefSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
